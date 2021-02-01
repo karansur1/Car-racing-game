99ed9ba039f48a2127c5d64a78c675848f0fe144
karansur1 <73495084+karansur1@users.noreply.github.com>
2021-02-01 14:24:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var car1I,car2I,car3I,car4I;
+var trackIMG
+
+
+function preload(){
+
+  car1I = loadImage("images/car1.png");
+  car2I = loadImage("images/car2.png");
+  car3I = loadImage("images/car3.png");
+  car4I = loadImage("images/car4.png");
+
+  trackIMG = loadImage("images/track.jpg")
+
+
+
+
+
+}
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+
+
+  if(gameState === 2){
+    game.end()
+
+  }
+}
